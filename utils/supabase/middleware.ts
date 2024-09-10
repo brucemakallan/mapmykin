b7f596e88c15bf0d1fea31598bf29758957e36e7
Bruce Allan Makaaru <brucemakallan@gmail.com>
2024-09-10 16:45:52 +0300

    Fix lint errors in utils and lib

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,5 +1,5 @@
-import { createServerClient } from "@supabase/ssr";
-import { type NextRequest, NextResponse } from "next/server";
+import { createServerClient } from '@supabase/ssr'
+import { type NextRequest, NextResponse } from 'next/server'
 
 export const updateSession = async (request: NextRequest) => {
   // This `try/catch` block is only here for the interactive tutorial.
@@ -10,46 +10,48 @@ export const updateSession = async (request: NextRequest) => {
       request: {
         headers: request.headers,
       },
-    });
+    })
 
     const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
       {
         cookies: {
           getAll() {
-            return request.cookies.getAll();
+            return request.cookies.getAll()
           },
           setAll(cookiesToSet) {
             cookiesToSet.forEach(({ name, value }) =>
-              request.cookies.set(name, value),
-            );
+              request.cookies.set(name, value)
+            )
             response = NextResponse.next({
               request,
-            });
+            })
             cookiesToSet.forEach(({ name, value, options }) =>
-              response.cookies.set(name, value, options),
-            );
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+              response.cookies.set(name, value, options)
+            )
           },
         },
-      },
-    );
+      }
+    )
 
     // This will refresh session if expired - required for Server Components
     // https://supabase.com/docs/guides/auth/server-side/nextjs
-    const user = await supabase.auth.getUser();
+    const user = await supabase.auth.getUser()
 
     // protected routes
-    if (request.nextUrl.pathname.startsWith("/protected") && user.error) {
-      return NextResponse.redirect(new URL("/sign-in", request.url));
+    if (request.nextUrl.pathname.startsWith('/protected') && user.error) {
+      return NextResponse.redirect(new URL('/sign-in', request.url))
     }
 
-    if (request.nextUrl.pathname === "/" && !user.error) {
-      return NextResponse.redirect(new URL("/protected", request.url));
+    if (request.nextUrl.pathname === '/' && !user.error) {
+      return NextResponse.redirect(new URL('/protected', request.url))
     }
 
-    return response;
-  } catch (e) {
+    return response
+  } catch (error) {
+    console.error(error)
     // If you are here, a Supabase client could not be created!
     // This is likely because you have not set up environment variables.
     // Check out http://localhost:3000 for Next Steps.
@@ -57,6 +59,6 @@ export const updateSession = async (request: NextRequest) => {
       request: {
         headers: request.headers,
       },
-    });
+    })
   }
-};
+}
